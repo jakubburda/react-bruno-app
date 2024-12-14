b72f3fda3595cac9c5c705dc31c35ec3c2ba54e4
Jakub Burda <jakubburdaofc@gmail.com>
2024-12-14 15:13:36 +0100

    feat(api): prepare data fetching with axios client
    - Set up axios client with base URL, authorization header, and timeout.
    - Implemented fetchDataFromApi function to handle API request and error handling.
    - Added support for environment variables to configure API URL and keys.
    - Included error handling for request timeouts and API call failures.

---
+++ src/api/api.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+    baseURL: process.env.REACT_APP_BRUNO_API_URL,
+    headers: {
+        'Authorization' : `Bearer ${process.env.REACT_APP_BRUNO_API_KEY}`,
+        'Content-Type': 'application/json',
+    },
+    timeout: 10000,
+});
+
+const fetchDataFromApi = async () => {
+    try {
+        const response = await apiClient.get(process.env.REACT_APP_API_GET_LINKS_ENDPOINT);
+        return response.data;
+    } catch(error) {
+        if (error.code === 'ECONNABORTED') {
+            console.error('Request timeout: The server took too long to respond.', error);
+          } else {
+            console.error('Error fetching links:', error);
+        }
+        throw Error;
+    }
+};
+
+export { fetchDataFromApi };
